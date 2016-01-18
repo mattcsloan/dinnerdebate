d2bf87e41d97e2d9be08a7a7baa54608196977bb
Matt Sloan <msloan@knotice.com>
2016-01-18 11:58:35 -0500

    added user recipes to dashboard view.

--- src/client/angular/dashboard/dashboard.ctrl.js
+++ src/client/angular/dashboard/dashboard.ctrl.js
@@ -6,10 +6,23 @@ angular.module('DashboardCtrl', []).controller('DashboardController', function(P
 
   $http.get('/api/recipes/by/user')
     .success(function(data) {
-      console.log(data);
+      vm.userRecipes = data;
+      usersOwnRecipes();
     })
     .error(function() {
       console.log('error');
     });
+
+
+  function usersOwnRecipes() {
+    vm.usersOwnRecipes = [];
+    angular.forEach(vm.userRecipes, function(item) {
+      //only add recipes that don't have an alternate source
+      console.log(item.sourceURL);
+      if(item.sourceURL == '' && item.source == '') {
+        vm.usersOwnRecipes.push(item);  
+      }
+    });
+  }
 
 });
