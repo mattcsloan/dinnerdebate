0ab6956afa3b8199a52b1124aa4fc3e6e74421d4
Matt Sloan <msloan@knotice.com>
2016-01-22 01:03:30 -0500

    updated redirect code to maintain querystring parameters. recipes page will now show querystring message if applicable.

--- src/client/angular/_common/controllers/main.ctrl.js
+++ src/client/angular/_common/controllers/main.ctrl.js
@@ -15,7 +15,7 @@ angular.module('MainCtrl', []).controller('MainController', function($state, $ro
   $rootScope.$on('$stateChangeStart', function(evt, to, params) {
     if (to.redirectTo) {
       evt.preventDefault();
-      $state.go(to.redirectTo, params)
+      $state.go(to.redirectTo, params, { location: false })
     } else if (to.external) {
       evt.preventDefault();
       $window.open(to.url, '_self');
