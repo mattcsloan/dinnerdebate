6cea94e0f3c6f5bac14fbbfa376bdd7e5f60c7fc
Matt Sloan <msloan@knotice.com>
2016-01-28 19:23:09 -0500

    removed bodyparser references from server.js

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ mongoose.connect(db.url);
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
-app.use(bodyParser.json()); 
+// app.use(bodyParser.json()); 
 
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
+// app.use(bodyParser.urlencoded({ extended: true })); 
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(methodOverride('X-HTTP-Method-Override')); 
