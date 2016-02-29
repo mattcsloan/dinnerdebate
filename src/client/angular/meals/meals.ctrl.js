331bbefa97c31871aeec3405a487cf591e3b88f3
Matt Sloan <msloan@knotice.com>
2016-02-29 00:40:40 -0500

    updated meals data to account for all elements inside meal overview modal

--- src/client/angular/meals/meals.ctrl.js
+++ src/client/angular/meals/meals.ctrl.js
@@ -6,12 +6,6 @@ angular.module('MealsCtrl', []).controller('MealsController', function(Page, $sc
 
   vm.day = moment();
 
-  // vm.modalShown = false;
-  // $scope.toggleModal = function(meal) {
-  //   $scope.mealDescription = meal;
-  //   vm.modalShown = !vm.modalShown;
-  // };
-
   vm.items = [
     [
       {
@@ -33,9 +27,115 @@ angular.module('MealsCtrl', []).controller('MealsController', function(Page, $sc
     ],
     [
       {
-        image: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1455987881/29205/DSC_1168-apricot-chicken-sm.jpg",
+        image: {
+          main: {
+            src: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1455987881/29205/DSC_1168-apricot-chicken-sm.jpg",
+            categoryKey: "entrees",
+            key: "apricot-chicken",
+            link: null
+          },
+          secondary: [{
+            src: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1455987881/29205/DSC_1168-apricot-chicken-sm.jpg",
+            categoryKey: "entrees",
+            key: "apricot-chicken",
+            link: null
+          }, {
+            src: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1455987881/29205/DSC_1168-apricot-chicken-sm.jpg",
+            categoryKey: "entrees",
+            key: "apricot-chicken",
+            link: null
+          }]
+        },
         entree: {
           title: "Apricot Chicken",
+          categoryKey: "entrees",
+          key: "apricot-chicken"
+        },
+        appetizers: [{
+          title: "broccoli",
+          categoryKey: null,
+          key: null
+        }, {
+          title: "broccoli",
+          categoryKey: null,
+          key: null
+        }],
+        sides: [{
+          title: "broccoli",
+          categoryKey: null,
+          key: null
+        }, {
+          title: "broccoli",
+          categoryKey: null,
+          key: null
+        }],
+        drinks: [{
+          title: "Milk",
+          categoryKey: null,
+          key: null
+        }],
+        desserts: [{
+          title: "Chocolate Chip Cookies",
+          categoryKey: null,
+          key: null
+        }],
+        prepTime: 35,
+        cookTime: 25,
+        mealUrl: "http://www.google.com",
+        published: true
+      },
+      {
+        image: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1454992240/96092/banana-bread.jpg",
+        entree: {
+          title: "Banana Bread",
+          categoryKey: "entrees",
+          key: "apricot-chicken"
+        },
+        appetizers: ["broccoli", "applesauce"],
+        sides: ["broccoli", "applesauce"],
+        drinks: ["Milk"],
+        desserts: ["Chocolate Chip Cookies"],
+        prepTime: 35,
+        cookTime: 25,
+        mealUrl: "http://www.google.com",
+        published: true
+      },
+      {
+        image: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1454606793/89532/sweet-and-sour-pork-chops.jpg",
+        entree: {
+          title: "Sweet & Sour Chicken",
+          categoryKey: "entrees",
+          key: "apricot-chicken"
+        },
+        appetizers: ["broccoli", "applesauce"],
+        sides: ["broccoli", "applesauce"],
+        drinks: ["Milk"],
+        desserts: ["Chocolate Chip Cookies"],
+        prepTime: 35,
+        cookTime: 25,
+        mealUrl: "http://www.google.com",
+        published: true
+      },
+      {
+        image: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1454605945/62774/green_chile_enchiladas.jpg",
+        entree: {
+          title: "Chicken Enchiladas",
+          categoryKey: "entrees",
+          key: "apricot-chicken"
+        },
+        appetizers: ["broccoli", "applesauce"],
+        sides: ["broccoli", "applesauce"],
+        drinks: ["Milk"],
+        desserts: ["Chocolate Chip Cookies"],
+        prepTime: 35,
+        cookTime: 25,
+        mealUrl: "http://www.google.com",
+        published: true
+      },
+      {
+        image: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1454967882/37591/DSC_1030-won-tons.jpg",
+        entree: {
+          title: "Won Tons",
           categoryKey: "entrees",
           key: "apricot-chicken"
         },
@@ -81,73 +181,9 @@ angular.module('MealsCtrl', []).controller('MealsController', function(Page, $sc
         published: true
       },
       {
-        image: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1455987881/29205/DSC_1168-apricot-chicken-sm.jpg",
+        image: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1454993417/42196/DSC_1043-chicken-basil-calzone.jpg",
         entree: {
-          title: "Apricot Chicken",
-          categoryKey: "entrees",
-          key: "apricot-chicken"
-        },
-        appetizers: ["broccoli", "applesauce"],
-        sides: ["broccoli", "applesauce"],
-        drinks: ["Milk"],
-        desserts: ["Chocolate Chip Cookies"],
-        prepTime: 35,
-        cookTime: 25,
-        mealUrl: "http://www.google.com",
-        published: true
-      },
-      {
-        image: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1455987881/29205/DSC_1168-apricot-chicken-sm.jpg",
-        entree: {
-          title: "Apricot Chicken",
-          categoryKey: "entrees",
-          key: "apricot-chicken"
-        },
-        appetizers: ["broccoli", "applesauce"],
-        sides: ["broccoli", "applesauce"],
-        drinks: ["Milk"],
-        desserts: ["Chocolate Chip Cookies"],
-        prepTime: 35,
-        cookTime: 25,
-        mealUrl: "http://www.google.com",
-        published: true
-      },
-      {
-        image: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1455987881/29205/DSC_1168-apricot-chicken-sm.jpg",
-        entree: {
-          title: "Apricot Chicken",
-          categoryKey: "entrees",
-          key: "apricot-chicken"
-        },
-        appetizers: ["broccoli", "applesauce"],
-        sides: ["broccoli", "applesauce"],
-        drinks: ["Milk"],
-        desserts: ["Chocolate Chip Cookies"],
-        prepTime: 35,
-        cookTime: 25,
-        mealUrl: "http://www.google.com",
-        published: true
-      },
-      {
-        image: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1455987881/29205/DSC_1168-apricot-chicken-sm.jpg",
-        entree: {
-          title: "Apricot Chicken",
-          categoryKey: "entrees",
-          key: "apricot-chicken"
-        },
-        appetizers: ["broccoli", "applesauce"],
-        sides: ["broccoli", "applesauce"],
-        drinks: ["Milk"],
-        desserts: ["Chocolate Chip Cookies"],
-        prepTime: 35,
-        cookTime: 25,
-        mealUrl: "http://www.google.com",
-        published: true
-      },
-      {
-        image: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1455987881/29205/DSC_1168-apricot-chicken-sm.jpg",
-        entree: {
-          title: "Apricot Chicken",
+          title: "Chicken & Basil Calzones",
           categoryKey: "entrees",
           key: "apricot-chicken"
         },
@@ -177,11 +213,11 @@ angular.module('MealsCtrl', []).controller('MealsController', function(Page, $sc
         published: true
       },
       {
-        image: null,
+        image: "http://res.cloudinary.com/sloan/image/upload/a_exif,c_fill,h_200,w_300/v1456634651/78013/DSC_1284-black-white-chili-sm.jpg",
         entree: {
-          title: null,
-          categoryKey: null,
-          key: null
+          title: "Black & White Chili",
+          categoryKey: "soups",
+          key: "black-and-white-chili-2"
         },
         appetizers: null,
         sides: null,
@@ -190,7 +226,7 @@ angular.module('MealsCtrl', []).controller('MealsController', function(Page, $sc
         prepTime: null,
         cookTime: null,
         mealUrl: null,
-        published: false
+        published: true
       },
       {
         image: null,
