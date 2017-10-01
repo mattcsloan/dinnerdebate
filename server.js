94e12e7f533057d71821e9b2b33a081b318ba6ab
Matt Sloan <sloan@Matts-MacBook-Pro.local>
2017-10-01 11:08:56 -0400

    Removed stormpath code for running app. Still need to remove from individual API calls and header.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // modules =================================================
 var express        = require('express');
-var stormpath      = require('express-stormpath')
+// var stormpath      = require('express-stormpath')
 var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
@@ -17,7 +17,7 @@ var port = process.env.PORT || 5000;
 
 
 // connect to our mongoDB database 
-mongoose.connect(db.url); 
+mongoose.connect(db.url, { useMongoClient: true });
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
@@ -33,29 +33,29 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride('X-HTTP-Method-Override')); 
 
 // initialize stormpath middleware
-app.use(stormpath.init(app, {
-  // Optional configuration options.
-  website: true,
-  expand: {
-    customData: true,
-    groups: true
-  },
-  web: {
-    login: {
-      enabled: true,
-      nextUri: "/dashboard"
-    },
-    register: {
-      autoLogin: true,
-      nextUri: '/dashboard'    // Where to send the user to, if auto login is enabled
-    },
-    logout: {
-      enabled: true,
-      nextUri: '/'
-    }
-  },
-  debug: 'info, error'
-}));
+// app.use(stormpath.init(app, {
+//   // Optional configuration options.
+//   website: true,
+//   expand: {
+//     customData: true,
+//     groups: true
+//   },
+//   web: {
+//     login: {
+//       enabled: true,
+//       nextUri: "/dashboard"
+//     },
+//     register: {
+//       autoLogin: true,
+//       nextUri: '/dashboard'    // Where to send the user to, if auto login is enabled
+//     },
+//     logout: {
+//       enabled: true,
+//       nextUri: '/'
+//     }
+//   },
+//   debug: 'info, error'
+// }));
 
 // app.use(multer({
 //   dest: __dirname + '/dist/uploads/'
@@ -75,11 +75,11 @@ require('./src/server/routes')(app); // configure our routes
 // start app ===============================================
 // startup our app at http://localhost:8080 once stormpath has initialized
 // Once Stormpath has initialized itself, start your web server!
-app.on('stormpath.ready', function () {
+// app.on('stormpath.ready', function () {
   var server = app.listen(port, function() {
     console.log('Running on port ' + port);
   });               
-});
+// });
 
 // expose app           
 exports = module.exports = app;
